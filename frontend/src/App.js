d552a7eafa591259653b1c0220f9d894ada177b9
wally-jnyor <arealunar9@gmail.com>
2021-10-23 11:53:28 -0300

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import { Route, Link } from 'react-router-dom'
@@ -8,27 +10,25 @@ import './App.css';
 
 const BaseLayout = () => (
   <div className="container-fluid">
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Django React Demo</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div className="navbar-nav">
-      <a className="nav-item nav-link" href="/">CUSTOMERS</a>
-      <a className="nav-item nav-link" href="/customer">CREATE CUSTOMER</a>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">Django React Demo</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link" href="/">CUSTOMERS</a>
+          <a className="nav-item nav-link" href="/customer">CREATE CUSTOMER</a>
 
-    </div>
-  </div>
-</nav>
+        </div>
+      </div>
+    </nav>
 
     <div className="content">
       <Route path="/" exact component={CustomersList} />
       <Route path="/customer/:pk"  component={CustomerCreateUpdate} />
       <Route path="/customer/" exact component={CustomerCreateUpdate} />
-
     </div>
-
   </div>
 )
 
